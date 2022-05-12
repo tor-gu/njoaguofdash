2ddf2203b28a358f6c4937f8bc27e114677fbbe0
tor-gu <tor.gunston@protonmail.com>
2022-05-12 12:32:40 -0400

    Added some comments

--- R/mod_filter.R
+++ R/mod_filter.R
@@ -1,15 +1,20 @@
 filterUI <- function(id) {
   ns <- NS(id)
   tagList(
+    # Geography selection
     radioButtons(ns("geography"), "Geography",
                 choices=c("State"="state", "County"="county"),
                 selected="state", inline=TRUE),
     selectInput(ns("county"), "County", counties),
+
+    # Table selection
     radioButtons(ns("table"), "Incidents or Subjects",
                 c("Incidents"="incident","Subjects"="subject")) %>%
       shinyhelper::helper(type="inline",
                           title="Incidents or Subjects",
                           content=filter_ui_table_help()),
+
+    # Filter selection
     selectInput(ns("incident_filter"), "Filter",
                 choices = filter_ui_make_filter_choice_list("incident")) %>%
       shinyhelper::helper(type = "inline",
@@ -20,6 +25,8 @@ filterUI <- function(id) {
       shinyhelper::helper(type = "inline",
                           title = "Filter",
                           content = filter_ui_filter_help()),
+
+    # Filter value selection
     selectInput(ns("filter_value"), "Filter Value",
                 choices=list("all"), selected="all"),
   )
@@ -27,6 +34,8 @@ filterUI <- function(id) {
 
 filterServer <- function(id) {
   moduleServer(id, function(input, output, session) {
+
+    # The county selection is only available on the county geography
     observe({
       shinyjs::toggleElement(
         "county",
@@ -38,6 +47,7 @@ filterServer <- function(id) {
       )
     })
 
+    # Switch the filter based on the table
     observe({
       shinyjs::toggleElement("incident_filter",
                              condition = {
@@ -57,6 +67,8 @@ filterServer <- function(id) {
                              })
     })
 
+    # When a filter is set, we ned to freeze the value selection until
+    # the values have been populated
     observe({
       if (filter_name() != "all") {
         freezeReactiveValue(input, "filter_value")
@@ -66,27 +78,33 @@ filterServer <- function(id) {
       }
     })
 
+    # Filter name
     filter_name <- reactive({
       if (input$table == "incident") input$incident_filter
       else input$subject_filter
     })
 
+    # Filter display name
     filter_display_name <- reactive({
       filter_server_get_filter_display_name(input$table, filter_name())
     })
 
+    # List of tables
     tables <- reactive({
       filter_server_get_tables(input$table, input$geography, input$county)
     })
 
+    # Summary table
     summary_table <- reactive({
       filter_server_get_summary_table(tables(), input$table, input$geography)
     })
 
+    # Table joined, when a multi-value filter is selected
     joined_table <- reactive({
       filter_server_get_joined_table(tables(), input$table, filter_name())
     })
 
+    # Table with filter applied
     filtered_table <- reactive({
       req(input$filter_value)
       filter_server_get_filtered_table(joined_table(), input$table,
